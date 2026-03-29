9ef52700a47e6c721885d4314b64f6219f003d63
Arjundhanush <arjundhanush64@gmail.com>
2026-03-29 19:32:46 +0530

    intermediate stage

---
+++ todo/backend/index.js
@@ -0,0 +1,20 @@
+const express =require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const app = express()
+const TodoModel = require('./models/todo')
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://192.168.216.1:27017/todolist')
+app.post('/add', (req, res) => {
+    const task=req.body.task;
+    TodoModel.create({task:task})
+    .then(result => res.json(result))
+    .catch(error => res.status(500).json({ error: 'Error adding task' }))   
+
+})
+
+app.listen(3001,()=>{
+    console.log('server is running on port 3001')
+})
--- todo/src/create.jsx
+++ todo/src/create.jsx
@@ -1,10 +1,17 @@
 import React from "react"
-
+import axios from "axios"
 function Create() {
+    const [task, setTask] = React.useState("")
+    const handleAdd = () => {
+        // Logic to add a new task
+        axios.post('http://localhost:3001/add', { task: task })
+        .then(result => console.log(result))
+        .catch(error => console.error('Error adding task:', error))
+    }
     return (
         <div>
-            <input type="text" placeholder="Add a new task" />  
-            <button>Add</button>
+            <input type="text" placeholder="Add a new task" onchange={(e) => setTask(e.target.value)}/>  
+            <button onclick={handleAdd}>Add</button>
         </div>
     )
 }
