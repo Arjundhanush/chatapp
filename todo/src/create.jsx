45e960aa5a6557db23c25fe0b9163b3a8b690422
Arjundhanush <163957659+Arjundhanush@users.noreply.github.com>
2026-04-21 20:04:02 +0530

    Change task addition API endpoint and fix handlers
    Updated the API endpoint for adding tasks and fixed event handler casing.

--- todo/src/create.jsx
+++ todo/src/create.jsx
@@ -1,17 +1,29 @@
-import React from "react"
+import React from "react";
+import axios from "axios";
+
 function Create() {
-    const [task, setTask] = React.useState("")
-    const handleAdd = () => {
-        // Logic to add a new task
-        axios.post('http://localhost:3001/add', { task: task })
-        .then(result => console.log(result))
-        .catch(error => console.error('Error adding task:', error))
-    }
-    return (
-        <div>
-            <input type="text" placeholder="Add a new task" onchange={(e) => setTask(e.target.value)}/>  
-            <button onclick={handleAdd}>Add</button>
-        </div>
+  const [task, setTask] = React.useState("");
+
+  const handleAdd = () => {
+    axios.post(
+      "https://todoapp-backend-bfj8.onrender.com/add",
+      { task: task }
     )
+    .then(result => console.log(result))
+    .catch(error => console.error("Error adding task:", error));
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Add a new task"
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button onClick={handleAdd}>Add</button>
+    </div>
+  );
 }
-export default Create
+
+export default Create;
