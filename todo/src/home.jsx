59b72bc43f2036ea4e92b8c38d00a364be2c88a5
Arjundhanush <163957659+Arjundhanush@users.noreply.github.com>
2026-04-21 20:02:48 +0530

    Update fetch URL for todos to production endpoint

--- todo/src/home.jsx
+++ todo/src/home.jsx
@@ -4,7 +4,7 @@ import Create from './create'
 function Home() {
   const [todo,setTodos] = useState([])
   useEffect(() => {
-    fetch('http://localhost:3001/todos')
+    fetch('https://todoapp-backend-bfj8.onrender.com/todos')
       .then(response => response.json())
       .then(data => setTodos(data))
       .catch(error => console.error('Error fetching todos:', error))
